e929434a044b5fbaa19d426931d9aa47c948c098
xelita <dev@xelita.com>
2020-02-02 21:52:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+console.log('electron main process...');
+
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: false,
+        frame: false,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/dist/electron-angular/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    win.once('ready-to-show', () => {
+        win.show();
+    });
+}
+
+app.on('closed', () => {
+    win = null;
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
+
+app.on('ready', createWindow);
